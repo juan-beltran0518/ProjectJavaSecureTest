1ce4d49d1a99fe04cfb6c547a28bd048fa4ae678
Sebastian Beltran <juan.beltran0518@hotmail.com>
2026-04-06 21:24:35 -0500

    Add Javadoc comment for HealthController class

--- src/main/java/com/spti/project/eci/HealthController.java
+++ src/main/java/com/spti/project/eci/HealthController.java
@@ -5,6 +5,9 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller for handling health check requests.
+ */
 @RestController
 public class HealthController {
 
