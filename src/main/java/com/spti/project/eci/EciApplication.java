4541549be9a882159bb4560a2bf23af71a54f2d5
Sebastian Beltran <juan.beltran0518@hotmail.com>
2026-04-08 17:46:09 -0500

    s

--- src/main/java/com/spti/project/eci/EciApplication.java
+++ src/main/java/com/spti/project/eci/EciApplication.java
@@ -3,9 +3,7 @@ package com.spti.project.eci;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Main class for the ECI application.
- */
+
 @SpringBootApplication
 public class EciApplication {
 
